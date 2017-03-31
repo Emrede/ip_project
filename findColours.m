094a2d52eb53b78367f4ebfec3afaadfa21a8569
Emrede <emrede@live.com>
2017-03-31 20:53:28 +0100

    Test commit

--- findColours.m
+++ findColours.m
@@ -1,7 +1,7 @@
 function colours = findColours(img)
     filter=fspecial('average',10);   %Create a filter
 	img=imfilter(img,filter);  %Apply filter
-    
+    %testsfdsfs
     figure, imshow(img), title('Noise Reduction');
     colours = repmat(0, [4 4 3]); %Create a 4x4x3 matrix to store the RGB values
     
